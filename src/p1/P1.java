8aa4555c684681be7d4f6a94380f5f4bccf62647
kazimdaqiqi <155591206+kazimdaqiqi@users.noreply.github.com>
2024-01-07 18:08:10 -0500

    Update P1.java

--- src/p1/P1.java
+++ src/p1/P1.java
@@ -4,5 +4,7 @@ public class P1 {
 	public static void main(String [] args) {
 		System.out.println("KAZIM DAQIQI..........");
 	}
+	public static void add(int a, int b){
+		System.out.println(a+b);
 
 }
